e7c41e7e882c73d45a18295c1bb5e586ccd3c418
fabiocaccamo <fabio.caccamo@gmail.com>
2021-05-17 14:26:18 +0200

    Allowed '()[]' chars in filenames when importing .rcjk files.

--- robocjk/io/paths.py
+++ robocjk/io/paths.py
@@ -10,11 +10,12 @@ FONT_PATTERN = r'((?P<font_name>[\w\-_\.]+)\.rcjk\/)'
 FONTLIB_PATTERN = r'^{}?fontLib\.json$'.format(FONT_PATTERN)
 FEATURES_PATTERN = r'^{}?features\.fea$'.format(FONT_PATTERN)
 DESIGNSPACE_PATTERN = r'^{}?designspace\.json$'.format(FONT_PATTERN)
-ATOMIC_ELEMENT_PATTERN = r'^{}?atomicElement\/(?P<glif_name>[\w\-\_\.\+]+)\.glif$'.format(FONT_PATTERN)
-ATOMIC_ELEMENT_LAYER_PATTERN = r'^{}?atomicElement\/(?P<layer_name>[\w\-\_\.\+]+)\/(?P<glif_name>[\w\-\_\.\+]+)\.glif$'.format(FONT_PATTERN)
-DEEP_COMPONENT_PATTERN = r'^{}?deepComponent\/(?P<glif_name>[\w\-\_\.\+]+)\.glif$'.format(FONT_PATTERN)
-CHARACTER_GLYPH_PATTERN = r'^{}?characterGlyph\/(?P<glif_name>[\w\-\_\.\+]+)\.glif$'.format(FONT_PATTERN)
-CHARACTER_GLYPH_LAYER_PATTERN = r'^{}?characterGlyph\/(?P<layer_name>[\w\-\_\.\+]+)\/(?P<glif_name>[\w\-\_\.\+]+)\.glif$'.format(FONT_PATTERN)
+NAME_PATTERN = r'[\w\-\_\.\+\(\)\[\]]+'
+ATOMIC_ELEMENT_PATTERN = r'^{}?atomicElement\/(?P<glif_name>{})\.glif$'.format(FONT_PATTERN, NAME_PATTERN)
+ATOMIC_ELEMENT_LAYER_PATTERN = r'^{}?atomicElement\/(?P<layer_name>{})\/(?P<glif_name>{})\.glif$'.format(FONT_PATTERN, NAME_PATTERN, NAME_PATTERN)
+DEEP_COMPONENT_PATTERN = r'^{}?deepComponent\/(?P<glif_name>{})\.glif$'.format(FONT_PATTERN, NAME_PATTERN)
+CHARACTER_GLYPH_PATTERN = r'^{}?characterGlyph\/(?P<glif_name>{})\.glif$'.format(FONT_PATTERN, NAME_PATTERN)
+CHARACTER_GLYPH_LAYER_PATTERN = r'^{}?characterGlyph\/(?P<layer_name>{})\/(?P<glif_name>{})\.glif$'.format(FONT_PATTERN, NAME_PATTERN, NAME_PATTERN)
 
 FONT_RE = re.compile(FONT_PATTERN)
 FONTLIB_RE = re.compile(FONTLIB_PATTERN)

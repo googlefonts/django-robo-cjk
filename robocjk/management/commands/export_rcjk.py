18756b50a00fe0bee0ca1281f20ed14d6cde771b
fabiocaccamo <fabio.caccamo@gmail.com>
2021-06-17 19:45:12 +0200

    Improved export command logging.

--- robocjk/management/commands/export_rcjk.py
+++ robocjk/management/commands/export_rcjk.py
@@ -2,7 +2,11 @@
 
 from django.core.management.base import BaseCommand
 
+from robocjk.debug import logger
 from robocjk.models import Project
+
+import os
+import threading
 
 
 class Command(BaseCommand):
@@ -10,6 +14,15 @@ class Command(BaseCommand):
     help = 'Export all .rcjk projects and push changes to their own git repositories.'
 
     def handle(self, *args, **options):
+
+        logger.info('-' * 100)
+        logger.info('Start export - pid: {} - thread: {}'.format(
+            os.getpid(), threading.current_thread().name))
+
         projects_qs = Project.objects.prefetch_related('fonts')
         for project in projects_qs:
             project.export()
+
+        logger.info('Complete export - pid: {} - thread: {}'.format(
+            os.getpid(), threading.current_thread().name))
+        logger.info('-' * 100)

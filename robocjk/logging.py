175127b4f9ea8fa0ba9b5d6a54bafbc2344d2c3a
fabiocaccamo <fabio.caccamo@gmail.com>
2023-04-06 15:50:59 +0200

    Fix logging filter throttling.

--- robocjk/logging.py
+++ robocjk/logging.py
@@ -1,5 +1,5 @@
+import hashlib
 import logging
-import traceback
 
 
 class ThrottleFilter(logging.Filter):
@@ -12,21 +12,11 @@ class ThrottleFilter(logging.Filter):
             from django.core.cache import caches
 
             cache = caches["logging"]
-            if record.exc_info:
-                exc_type = record.exc_info[0]
-                exc_value = record.exc_info[1]
-                exc_traceback = exc_value.__traceback__
-                lastframe = traceback.extract_tb(exc_traceback)[-1]
-                key = (
-                    "logging.ThrottleFilter "
-                    f"{exc_type.__name__} "
-                    f"{lastframe.filename} "
-                    f"{lastframe.lineno} "
-                    f"{lastframe.name}"
-                )
-                if cache.get(key):
-                    return False
-                cache.set(key, True, self.timeout)
+            message = record.getMessage()
+            key = hashlib.md5(message.encode("utf-8")).hexdigest()
+            if cache.get(key):
+                return False
+            cache.set(key, True, self.timeout)
         except Exception:
             pass
         return True
